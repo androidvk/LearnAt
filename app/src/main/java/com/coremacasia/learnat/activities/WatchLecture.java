00d5370e7d68dec4e4f59bf2c5c71a953a932d45
Vikash Sharma <vk.sharma920@gmail.com>
2021-10-18 22:32:27 +0530

    Changes

---
+++ app/src/main/java/com/coremacasia/learnat/activities/WatchLecture.java
@@ -0,0 +1,16 @@
+package com.coremacasia.learnat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coremacasia.learnat.R;
+
+public class WatchLecture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_lecture);
+    }
+}
